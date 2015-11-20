553cffc893040f9e492c19e868fa87c51122d06c
chenhui <hui.chen@hjwang.com>
2015-11-20 11:43:51 +0800

    添加工具类、基类

--- src/com/basedamo/utils/ToastUtil.java
+++ src/com/basedamo/utils/ToastUtil.java
@@ -3,11 +3,13 @@ package com.basedamo.utils;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.basedamo.MyApplication;
+
 /**
  * Created by hui on 2015/11/20.
  */
 public class ToastUtil {
-    public static void show(Context context,String msg){
-        Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+    public static void show(String msg){
+        Toast.makeText(MyApplication.getContext(),msg,Toast.LENGTH_LONG).show();
     }
 }
