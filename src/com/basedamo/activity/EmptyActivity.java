148f3f9dedc22bd289949e3e68bb9a5fdef24009
chenhui <hui.chen@hjwang.com>
2015-11-27 16:18:39 +0800

    添加科大讯飞jar包

---
+++ src/com/basedamo/activity/EmptyActivity.java
@@ -0,0 +1,26 @@
+package com.basedamo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.basedamo.BaseActivity;
+
+/**
+ * Created by hui on 2015/11/27.
+ */
+public class EmptyActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
