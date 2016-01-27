0b3e69921bf18e6a7d4b9a972a9f88e0b42a32d9
chenhui <c2xxx@qq.com>
2016-01-27 11:36:23 +0800

    添加gradle脚本练习

--- src/com/basedamo/activity/GradleStudyActivity.java
+++ src/com/basedamo/activity/GradleStudyActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.basedamo.BaseActivity;
+import com.basedamo.BuildConfig;
 import com.basedamo.R;
 import com.basedamo.utils.ReadMainFestsInfo;
 
@@ -36,6 +37,17 @@ public class GradleStudyActivity extends BaseActivity {
         sb.append("\n versioncode=" + versioncode);
         sb.append("\n currentapiVersion=" + currentapiVersion);
         sb.append("\n UMENG_CHANNEL=" + UMENG_CHANNEL);
+
+
+        sb.append("\n BuildConfig.DEBUG=" + BuildConfig.DEBUG);
+        sb.append("\n BuildConfig.VERSION_NAME=" + BuildConfig.VERSION_NAME);
+        sb.append("\n BuildConfig.VERSION_CODE=" + BuildConfig.VERSION_CODE);
+        sb.append("\n BuildConfig.FLAVOR=" + BuildConfig.FLAVOR);
+        sb.append("\n BuildConfig.CHEN_VERSION_NAME=" + BuildConfig.CHEN_VERSION_NAME);
+        sb.append("\n BuildConfig.CHEN_DEBUG=" + BuildConfig.CHEN_DEBUG);
+        sb.append("\n BuildConfig.BUILD_TYPE=" + BuildConfig.BUILD_TYPE);
+        sb.append("\n BuildConfig.BUILD_TIME=" + BuildConfig.BUILD_TIME);
+
 
         tvMsg.setText(sb);
     }
